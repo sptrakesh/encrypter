fc05a6c01a0f6652ad001a997610595598469839
Rakesh Vidyadharan <rakesh@sptci.com>
2023-10-03 19:06:07 -0500

    Update python client to support encrypting and decrypting bytes

--- client/python/features/steps/service.py
+++ client/python/features/steps/service.py
@@ -1,3 +1,4 @@
+from base64 import b64encode, b64decode
 from behave import given, then, step
 from behave.api.async_step import async_run_until_complete
 from behave.runner import Context
@@ -37,3 +38,29 @@ async def step_impl(context: Context):
     async with _Client(host=context.host, port=context.port) as client:
         value = await client.decrypt(context.encrypted)
         assert_that(value, equal_to(context.value), "Decrypted value not same as original")
+
+
+@step("Encrypt some bytes")
+@async_run_until_complete
+async def step_impl(context: Context):
+    assert_that(hasattr(context, "host"), equal_to(True), "Encrypter service host not set")
+    assert_that(hasattr(context, "port"), equal_to(True), "Encrypter service port not set")
+    assert_that(hasattr(context, "value"), equal_to(True), "String to encrypt not set")
+
+    async with _Client(host=context.host, port=context.port) as client:
+        encoded = b64encode(context.value.encode("utf-8"))
+        value = await client.encrypt_bytes(encoded)
+        assert_that(len(value), not_(equal_to(0)), "Encrypted value of zero length")
+        context.encrypted = value
+
+
+@step("Decrypt the encrypted string back to bytes")
+@async_run_until_complete
+async def step_impl(context):
+    assert_that(hasattr(context, "host"), equal_to(True), "Encrypter service host not set")
+    assert_that(hasattr(context, "port"), equal_to(True), "Encrypter service port not set")
+    assert_that(hasattr(context, "value"), equal_to(True), "String to encrypt not set")
+
+    async with _Client(host=context.host, port=context.port) as client:
+        value = await client.decrypt_bytes(context.encrypted)
+        assert_that(b64decode(value).decode("utf-8"), equal_to(context.value), "Decrypted value not same as original")
